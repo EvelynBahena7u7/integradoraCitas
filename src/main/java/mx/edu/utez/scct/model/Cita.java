f6f16dd1c0ed782912ade269a994ce14c8ef8107
20193tn107@utez.edu.mx <20193tn107@utez.edu.mx>
2022-04-15 00:20:20 -0500

    cambios

--- src/main/java/mx/edu/utez/scct/model/Cita.java
+++ src/main/java/mx/edu/utez/scct/model/Cita.java
@@ -1,5 +1,6 @@
 package mx.edu.utez.scct.model;
 
+import java.util.Date;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -11,6 +12,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.JoinColumn;
 
 
@@ -22,14 +26,21 @@ public class Cita {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idCita;
 
-	@Column(nullable = false, length = 45)
-	private String fecha;
+	@Column(name = "fecha_registro", nullable = false)
+	@CreationTimestamp
+	private Date fecha;
 	
 	@Column(nullable = false, length = 45)
 	private String hora;
 	
+	@Column(nullable = false, length = 45)
+	private String horaFinal;
+	
 	@Column(nullable = false)
 	private boolean estado;
+	
+	@Column(nullable = false)
+	private int nVentanilla;
 
 
 	@ManyToMany(fetch = FetchType.EAGER)
@@ -44,52 +55,88 @@ public class Cita {
 		super();
 	}
 
-	public Cita(Long idCita, String fecha, String hora, boolean estado, Set<Documento> documento) {
-		super();
-		this.idCita = idCita;
-		this.fecha = fecha;
-		this.hora = hora;
-		this.estado = estado;
-		this.documento = documento;
-	}
 
 	public Long getIdCita() {
 		return idCita;
 	}
 
+
 	public void setIdCita(Long idCita) {
 		this.idCita = idCita;
 	}
+	
+	
 
-	public String getFecha() {
+
+	public Date getFecha() {
 		return fecha;
 	}
 
-	public void setFecha(String fecha) {
+
+	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
+
 
 	public String getHora() {
 		return hora;
 	}
 
+
 	public void setHora(String hora) {
 		this.hora = hora;
 	}
+
+
+	public String getHoraFinal() {
+		return horaFinal;
+	}
+
+
+	public void setHoraFinal(String horaFinal) {
+		this.horaFinal = horaFinal;
+	}
+
 
 	public boolean isEstado() {
 		return estado;
 	}
 
+
 	public void setEstado(boolean estado) {
 		this.estado = estado;
 	}
 
+
+	public int getnVentanilla() {
+		return nVentanilla;
+	}
+
+
+	public void setnVentanilla(int nVentanilla) {
+		this.nVentanilla = nVentanilla;
+	}
+
+
+	public Set<Documento> getDocumento() {
+		return documento;
+	}
+
+
+	public void setDocumento(Set<Documento> documento) {
+		this.documento = documento;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Cita [idCita=" + idCita + ", fecha=" + fecha + ", hora=" + hora + ", estado=" + estado + ", documento="
-				+ documento + "]";
+		return "Cita [idCita=" + idCita + ", fecha=" + fecha + ", hora=" + hora + ", horaFinal=" + horaFinal
+				+ ", estado=" + estado + ", nVentanilla=" + nVentanilla + ", documento=" + documento + "]";
 	}
+
+
+	
+
 	
 	
 }
